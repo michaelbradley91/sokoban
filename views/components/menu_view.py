84a7c49c4d4961c3d9278cd14dcda085bc1d1877
Michael Bradley <michael.bradley@hotmail.co.uk>
2020-01-02 19:38:36 +0000

    Using menu view to strip out common logic from start view

--- views/components/menu_view.py
+++ views/components/menu_view.py
@@ -1,16 +1,54 @@
+from typing import List
+
 from app_container import AppContainer
+from constants.colours import MENU_TEXT_COLOUR, MENU_SELECTED_TEXT_COLOUR
+from constants.numbers import MENU_TEXT_SELECTED_MARGIN, MENU_TEXT_MARGIN
 from coordinate import Coordinate
 from grid import Grid
 from layouts.aspect_layout import AspectLayout
 from layouts.grid_layout import GridLayout
 from layouts.layout import BasicLayout
+from layouts.margin_layout import MarginLayout
 from pieces.crate import CratePiece
 from pieces.piece_draw_order import PIECE_DRAW_ORDER
 from pieces.player import PlayerPiece
 from pieces.static import StaticPiece
+from resources import Resources
 from views.components.component_view import ComponentView
 from views.components.title_view import TitleView
-from views.start_view import TITLE_HEIGHT
+
+TITLE_HEIGHT = 2
+
+
+class MenuTextItem:
+    """
+    A simple menu item that can be highlighted or not
+    """
+    def __init__(self, resources: Resources, text: str, margin_layout: MarginLayout, layout: BasicLayout):
+        self.resources = resources
+        self.__margin_layout = margin_layout
+        self.text = text
+        self.__layout = layout
+        self.__highlighted = False
+
+    @property
+    def highlighted(self):
+        return self.__highlighted
+
+    @highlighted.setter
+    def highlighted(self, value):
+        # Update the margin
+        if value != self.__highlighted:
+            if value:
+                self.__margin_layout.set_margin((0, MENU_TEXT_SELECTED_MARGIN))
+            else:
+                self.__margin_layout.set_margin((0, MENU_TEXT_MARGIN))
+            self.__margin_layout.update_components()
+        self.__highlighted = value
+
+    def draw(self):
+        colour = MENU_SELECTED_TEXT_COLOUR if self.highlighted else MENU_TEXT_COLOUR
+        self.resources.menu_font.draw_text(self.text, colour, self.__layout.bounding_rect)
 
 
 class MenuView(ComponentView[BasicLayout]):
@@ -22,6 +60,7 @@ class MenuView(ComponentView[BasicLayout]):
     def __init__(self, app_container: AppContainer, layout: BasicLayout, grid: Grid):
         super().__init__(app_container, layout)
 
+        self.__menu_items: List[MenuTextItem] = []
         self.__grid = grid
 
         # Add the walls
@@ -53,6 +92,40 @@ class MenuView(ComponentView[BasicLayout]):
         aspect_layout.set_layout(self.grid_layout)
         self.layout.set_layout(aspect_layout)
 
+    def add_menu_item(self, text: str, position: Coordinate, column_span: int, row_span: int = 1) -> MenuTextItem:
+        """
+        Add a menu item to the view. Fills the grid and will draw appropriately.
+        :param text: the text to draw
+        :param position: where to position it on the grid's coordinates
+        :param column_span: the column span
+        :param row_span: the row span (defaults to 1)
+        :return: the menu item added
+        """
+
+        # Fill the appropriate portion of the grid with menu wall
+        for x_offset in range(column_span):
+            for y_offset in range(row_span):
+                coordinate = position + Coordinate(x_offset, y_offset)
+                piece = StaticPiece(self.__grid, self.app_container, self.resources.menu_background)
+                self.grid.add_piece(piece, coordinate)
+
+        # Create the menu item
+        margin_layout = MarginLayout(MENU_TEXT_MARGIN)
+        basic_layout = BasicLayout()
+        menu_text_item = MenuTextItem(self.resources, text, margin_layout, basic_layout)
+
+        surface = self.resources.menu_font.get_surface(text, MENU_TEXT_COLOUR)
+        # Technically assumes the aspect ratio does not change with colour
+        aspect_layout = AspectLayout((surface.get_width(), surface.get_height()))
+        aspect_layout.set_layout(basic_layout)
+
+        margin_layout.set_layout(aspect_layout)
+
+        self.grid_layout.add_layout(margin_layout, position, column_span=column_span, row_span=row_span)
+
+        self.__menu_items.append(menu_text_item)
+        return menu_text_item
+
     def draw_static(self):
         # Draw the grid
         for piece_type in [p for p in PIECE_DRAW_ORDER if p not in [PlayerPiece, CratePiece]]:
@@ -62,8 +135,21 @@ class MenuView(ComponentView[BasicLayout]):
         # Draw the title
         self.title_view.draw_static()
 
+        # Draw the menu
+        for menu_item in self.__menu_items:
+            menu_item.draw()
+
     def draw_animated(self):
-        pass
+        # Draw animated crates and players
+        for piece in self.grid.get_pieces_of_type(CratePiece):
+            piece.draw(self.grid_layout.bounding_rect.topleft, self.square_size)
+
+        for piece in self.grid.get_pieces_of_type(PlayerPiece):
+            piece.draw(self.grid_layout.bounding_rect.topleft, self.square_size)
+
+    @property
+    def menu_items(self) -> List[MenuTextItem]:
+        return list(self.__menu_items)
 
     @property
     def square_size(self):
--- views/start_view.py
+++ views/start_view.py
@@ -7,26 +7,22 @@ from pygame.event import EventType
 
 from animations.preset_animation import PresetGridAnimationPlayer, PresetGridAnimation
 from app_container import AppContainer
-from constants.colours import MENU_TEXT_COLOUR, TITLE_COLOUR, BACKGROUND_COLOUR, MENU_SELECTED_TEXT_COLOUR, \
-    TITLE_SHADOW_COLOUR
+from constants.colours import MENU_TEXT_COLOUR, BACKGROUND_COLOUR, MENU_SELECTED_TEXT_COLOUR
 from constants.direction import Direction
 from constants.numbers import MENU_TEXT_SELECTED_MARGIN, MENU_TEXT_MARGIN
-from constants.text import START_VIEW_START, START_VIEW_OPTIONS, START_VIEW_HELP, START_VIEW_QUIT, START_VIEW_TITLE, \
-    draw_text_with_border
+from constants.text import START_VIEW_START, START_VIEW_OPTIONS, START_VIEW_HELP, START_VIEW_QUIT
 from coordinate import Coordinate
 from grid import Grid
 from layouts.aspect_layout import AspectLayout
-from layouts.grid_layout import GridLayout
 from layouts.layout import BasicLayout
 from layouts.margin_layout import MarginLayout
 from opengl_support.helpers import set_background_and_clear
 from pieces.crate import CratePiece
 from pieces.goal import GoalPiece
-from pieces.piece_draw_order import PIECE_DRAW_ORDER
 from pieces.player import PlayerPiece
 from pieces.static import StaticPiece
 from pieces.wall import WallPiece
-from views.components.title_view import TitleView
+from views.components.menu_view import MenuView, TITLE_HEIGHT, MenuTextItem
 from views.view import ViewModel, View
 
 
@@ -42,7 +38,6 @@ MENU_INDEX_BY_OPTION = {m: i for i, m in enumerate(MenuOption)}
 # noinspection PyTypeChecker
 MENU_OPTION_BY_INDEX = {i: m for i, m in enumerate(MenuOption)}
 
-TITLE_HEIGHT = 2
 GRID_WIDTH = 12
 GRID_HEIGHT = TITLE_HEIGHT + 2 + (2 * len(MENU_OPTION_BY_INDEX)) + 1
 MENU_OPTION_WIDTH = 4
@@ -64,6 +59,7 @@ class StartViewParameters:
 class StartViewModel(ViewModel[StartViewParameters]):
     def __init__(self, view: "StartView"):
         super().__init__(view)
+        self.view: "StartView" = view
         self.menu_option_selected = MenuOption.start
 
         self.undo_manager.enabled = False
@@ -80,27 +76,11 @@ class StartViewModel(ViewModel[StartViewParameters]):
 
         self.load_grid()
         self.undo_manager.enabled = True
-        self.undo_manager.save_position(START_MENU_POSITION)
 
     def load_grid(self):
-        self.grid.add_outer_wall()
-
         # Player
         player_animation = PresetGridAnimation(Coordinate(MENU_OPTION_LEFT - 2, menu_option_position(0)))
         self.grid.add_piece(self.player_piece, player_animation.steps[0])
-
-        # Title bricks
-        for y in range(1, TITLE_HEIGHT + 1):
-            for x in range(1, self.grid.width - 1):
-                self.grid.add_piece(StaticPiece(self.grid, self.app_container, self.resources.menu_background),
-                                    Coordinate(x, y))
-
-        # Menu item bricks
-        for x in range(MENU_OPTION_LEFT, MENU_OPTION_RIGHT + 1):
-            for i in range(0, len(MENU_OPTION_BY_INDEX)):
-                y = menu_option_position(i)
-                self.grid.add_piece(StaticPiece(self.grid, self.app_container, self.resources.menu_background),
-                                    Coordinate(x, y))
 
         # Regular wall. We alternate blocked off positions every 2 menu items, starting with the second...
         for i in range(1, len(MENU_OPTION_BY_INDEX), 2):
@@ -153,8 +133,10 @@ class StartViewModel(ViewModel[StartViewParameters]):
         :return: nothing
         """
         index = MENU_INDEX_BY_OPTION[self.menu_option_selected]
+        self.menu_items[index].highlighted = False
         index = (index + 1) % len(MENU_INDEX_BY_OPTION)
         self.menu_option_selected = MENU_OPTION_BY_INDEX[index]
+        self.menu_items[index].highlighted = True
 
     def go_up_one_menu_option(self):
         """
@@ -162,8 +144,18 @@ class StartViewModel(ViewModel[StartViewParameters]):
         :return:
         """
         index = MENU_INDEX_BY_OPTION[self.menu_option_selected]
+        self.menu_items[index].highlighted = False
         index = (index + len(MENU_INDEX_BY_OPTION) - 1) % len(MENU_INDEX_BY_OPTION)
         self.menu_option_selected = MENU_OPTION_BY_INDEX[index]
+        self.menu_items[index].highlighted = True
+
+    @property
+    def menu_items(self) -> List[MenuTextItem]:
+        return self.view.menu_view.menu_items
+
+    @property
+    def menu_index_selected(self):
+        return MENU_INDEX_BY_OPTION[self.menu_option_selected]
 
 
 class StartView(View[StartViewParameters, StartViewModel]):
@@ -179,52 +171,32 @@ class StartView(View[StartViewParameters, StartViewModel]):
             MenuOption.quit: lambda: self.navigator.quit()
         }
 
-        self.menu_layouts: Dict[MenuOption, BasicLayout] = {m: BasicLayout() for m in MenuOption}
-        self.menu_margin_layouts: Dict[MenuOption, MarginLayout] = {m: MarginLayout((0, 1 / 5)) for m in MenuOption}
-        self.title_view: Optional[TitleView] = None
-        self.grid_layout = GridLayout(width=GRID_WIDTH, height=GRID_HEIGHT)
-        self.square_layout = BasicLayout()
+        self.menu_view: Optional[MenuView] = None
         self.position_won_time = None
         self.position_start_time = None
 
     def init(self):
-        # Add the title
-        title_container_layout = BasicLayout(identifier="title_layout")
-        self.title_view = TitleView(self.app_container, title_container_layout)
-        self.grid_layout.add_layout(title_container_layout, Coordinate(1, 1),
-                                    column_span=GRID_WIDTH - 2, row_span=TITLE_HEIGHT)
+        menu_container_layout = BasicLayout()
+        self.menu_view = MenuView(self.app_container, menu_container_layout, self.model.grid)
 
-        # Build layouts to hold the menu text
+        # Add menu items
         for i, m in MENU_OPTION_BY_INDEX.items():
-            surface = self.resources.menu_font.get_surface(m.value, MENU_TEXT_COLOUR)
-            aspect_layout = AspectLayout((surface.get_width(), surface.get_height()))
-            aspect_layout.set_layout(self.menu_layouts[m])
-
-            self.menu_margin_layouts[m].set_layout(aspect_layout)
-
             y = menu_option_position(i)
             x = MENU_OPTION_LEFT
+            self.menu_view.add_menu_item(m.value, Coordinate(x, y), column_span=MENU_OPTION_WIDTH)
 
-            self.grid_layout.add_layout(self.menu_margin_layouts[m], Coordinate(x, y), column_span=MENU_OPTION_WIDTH)
+        self.menu_view.menu_items[0].highlighted = True
 
-        self.grid_layout.add_layout(self.square_layout, Coordinate(0, 0))
-
-        aspect_layout = AspectLayout((self.grid_layout.grid_width, self.grid_layout.grid_height),
-                                     (self.grid_layout.grid_width, self.grid_layout.grid_height))
-
-        aspect_layout.set_layout(self.grid_layout)
-        self.layout.set_layout(aspect_layout)
+        self.layout.set_layout(menu_container_layout)
         self.position_start_time = time()
-
         self.resources.crate_sound.set_volume(0)
+
+        self.undo_manager.save_position(START_MENU_POSITION)
 
     def close(self):
         pass
 
     def pre_event_loop(self):
-        pass
-
-    def post_event_loop(self):
         pass
 
     def on_events(self, events: List[EventType]):
@@ -240,30 +212,12 @@ class StartView(View[StartViewParameters, StartViewModel]):
                 elif event.key == pygame.K_UP:
                     self.model.go_up_one_menu_option()
 
+    def post_event_loop(self):
+        pass
+
     def draw_static(self):
-        for m, margin_layout in self.menu_margin_layouts.items():
-            if self.model.menu_option_selected == m:
-                margin_layout.set_margin((0, MENU_TEXT_SELECTED_MARGIN))
-            else:
-                margin_layout.set_margin((0, MENU_TEXT_MARGIN))
-            margin_layout.update_components()
-
         set_background_and_clear(BACKGROUND_COLOUR)
-
-        # Draw the grid
-        for piece_type in [p for p in PIECE_DRAW_ORDER if p not in [PlayerPiece, CratePiece]]:
-            for piece in self.grid.get_pieces_of_type(piece_type):
-                piece.draw(self.grid_layout.bounding_rect.topleft, self.square_size)
-
-        # Draw the text on top
-        for m in MenuOption:
-            layout = self.menu_layouts[m]
-            text = m.value
-            colour = MENU_SELECTED_TEXT_COLOUR if m == self.model.menu_option_selected else MENU_TEXT_COLOUR
-            self.resources.menu_font.draw_text(text, colour, layout.bounding_rect)
-
-        # Draw the title
-        self.title_view.draw()
+        self.menu_view.draw_static()
 
     def post_animation_loop(self):
         if self.animator.animating():
@@ -287,16 +241,12 @@ class StartView(View[StartViewParameters, StartViewModel]):
             self.model.player_piece.move(self.model.player_animation_player.play_next_step())
 
     def draw_animated(self):
-        for piece in self.grid.get_pieces_of_type(CratePiece):
-            piece.draw(self.grid_layout.bounding_rect.topleft, self.square_size)
-
-        for piece in self.grid.get_pieces_of_type(PlayerPiece):
-            piece.draw(self.grid_layout.bounding_rect.topleft, self.square_size)
-
-    @property
-    def square_size(self):
-        return self.square_layout.bounding_rect.width
+        self.menu_view.draw_animated()
 
     @property
     def grid(self):
         return self.model.grid
+
+    @property
+    def grid_layout(self):
+        return self.menu_view.grid_layout
